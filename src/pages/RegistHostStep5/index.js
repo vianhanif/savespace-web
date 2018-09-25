d99b860be5c02cb270157b8c8e8d633dce608f76
Alvian Rahman Hanif <alvian524@gmail.com>
2018-09-25 15:55:09 +0700

    :sparkles: regist step 5 UI

---
+++ src/pages/RegistHostStep5/index.js
@@ -0,0 +1,59 @@
+import React, {Component, Fragment} from 'react';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {Container} from '../../components/Layout/Layout';
+import {GUTTER_TYPE} from '../../components/Layout/LayoutStyled';
+import Loader from '../../components/Loader';
+import Header from '../../components/Header'
+import Button from '../../components/Button'
+import * as appActions from '../../_actions/App'
+import Text from '../../components/Form/Text'
+import Select from '../../components/Form/Select'
+import './style.scss';
+
+class RegistHostStep5 extends Component {
+  state = {
+    fetched: true
+  };
+
+  componentWillMount() {
+    this.props.dispatch(appActions.setTitle('Intro'))
+  }
+
+  render() {
+    return this.state.fetched ? (
+      <Fragment>
+        <Header title={this.props.App.title}/>
+        <Container gutter={GUTTER_TYPE.BOTH}>
+          <div className="regist-step1-container">
+            <label className="title">
+              Lokasi
+            </label>
+            <div className="regist-form">
+              <Text type="text" placeholder="Alamat"></Text>
+              <Select options={[]} placeholder="Kota"/>
+              <Select options={[]} placeholder="Negara"/>
+            </div>
+            <div className="options">
+              <div className="left">
+                <Button>Kembali</Button>
+              </div>
+              <div className="right">
+                <Button onClick={() => this.props.history.push('/regist_host_step6')}>Ok</Button>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </Fragment>
+    ) : <Loader/>
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  dispatch
+});
+const mapStateToProps = state => ({
+    User: state.User,
+    App: state.App
+});
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(RegistHostStep5));
--- src/routers/routerComponents.js
+++ src/routers/routerComponents.js
@@ -17,7 +17,8 @@ export const routes = [
   {path: '/regist_host_step1', component: require('../pages/RegistHostStep1').default},
   {path: '/regist_host_step2', component: require('../pages/RegistHostStep2').default},
   {path: '/regist_host_step3', component: require('../pages/RegistHostStep3').default},
-  {path: '/regist_host_step4', component: require('../pages/RegistHostStep4').default}
+  {path: '/regist_host_step4', component: require('../pages/RegistHostStep4').default},
+  {path: '/regist_host_step5', component: require('../pages/RegistHostStep5').default}
 ];
 let routesScrollToTop = [];
 routes.forEach(item => {
